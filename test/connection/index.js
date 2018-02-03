faaea01e96e3688043b311afaf61450e1664a8e9
Rajat Kumar <rajatk@netflix.com>
2018-02-02 17:15:17 -0800

    feat: connection establishing API with tests

---
+++ test/connection/index.js
@@ -0,0 +1,116 @@
+'use strict';
+
+// external modules
+const assert = require('chai').assert;
+
+// internal modules
+const harperdb = require('../../lib');
+const username = 'HDB_ADMIN';
+const password = 'admin';
+const host = 'http://localhost';
+const port = '9925';
+
+/* eslint-disable no-console */
+
+describe('harperdb', function() {
+    it('should connect automatically', function(done) {
+        const client = harperdb.createClient({
+            host,
+            port,
+            username,
+            password,
+            autoverify: true
+        });
+
+        client.on('connection:success', function() {
+            const state = client.getCurrentState();
+            // must be connected
+            assert.isTrue(state.connected);
+            // must have the user info available
+            assert.isOk(state.users[username]);
+            done();
+        });
+
+        client.on('connection:failure', function(err) {
+            done(err);
+        });
+    });
+
+    it('should lazy connect', function(done) {
+        const client = harperdb.createClient({
+            host,
+            port,
+            username,
+            password,
+            autoverify: false
+        });
+
+        client.connect((err, state) => {
+            // no errors
+            assert.isNull(err);
+            // must be connected
+            assert.isTrue(state.connected);
+            // must have the user info available
+            assert.isOk(state.users[username]);
+            done();
+        });
+    });
+
+    it('should error on invalid user', function(done) {
+        const client = harperdb.createClient({
+            host,
+            port,
+            username: 'DrDrake',
+            password,
+            autoverify: false
+        });
+
+        client.connect((err, state) => {
+            console.log(err);
+            // assert it throws Error
+            assert.isNotNull(err);
+            assert.equal(err.name, 'UnauthorizedError');
+            done();
+        });
+    });
+
+    it('should error on invalid password', function(done) {
+        const client = harperdb.createClient({
+            host,
+            port,
+            username,
+            password: 'invalidofcourse',
+            autoverify: false
+        });
+
+        client.connect((err, state) => {
+            console.log(err);
+            // assert it throws Error
+            assert.isNotNull(err);
+            assert.equal(err.name, 'UnauthorizedError');
+            done();
+        });
+    });
+
+    it('should not connect for unavailable server', function(done) {
+        this.timeout(17000);
+        const client = harperdb.createClient({
+            host,
+            port: '9909',
+            username,
+            password,
+            autoverify: false,
+            connectTimeout: 5000,
+            requestTimeout: 5000
+        });
+
+        client.connect((err, state) => {
+            console.log(err);
+            // assert it throws Error
+            assert.isNotNull(err);
+            done();
+        });
+    });
+});
+
+/* eslint-enable no-console */
--- test/index.js
+++ test/index.js
@@ -1,11 +1,3 @@
 'use strict';
 
-// external modules
-const assert = require('chai').assert;
-const harperdb = require('../lib');
-
-describe('harperdb', function() {
-    it('should be awesome', function() {
-        assert.equal(5, harperdb.sample(2, 3));
-    });
-});
+require('./connection');
